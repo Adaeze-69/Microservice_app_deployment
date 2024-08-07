74e8c223e3d6127a93f8deca8f69868df9ee787d
Adaeze-69 <aadaezennamdi4@gmail.com>
2024-08-07 23:30:25 +0100

    github actions

--- Jenkins_server/variable.tf
+++
@@ -1,7 +0,0 @@
-variable "subnet_cidr_block" {}
-variable "vpc_cidr_block" {}
-variable "avail_zone" {}
-variable "env_prefix" {}
-variable "instance_type" {}
-variable "ami" {}
-variable "key_name" {}
--- sock_shop_eks_cluster/main.tf
+++ sock_shop_eks_cluster/main.tf
@@ -11,10 +11,6 @@ data "aws_availability_zones" "available" {
   }
 }
 
-locals {
-  cluster_name = "ada-eks-${random_string.suffix.result}"
-}
-
 resource "random_string" "suffix" {
   length  = 5
   special = false
@@ -49,24 +45,17 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "20.8.5"
 
-  cluster_name    = local.cluster_name
+  cluster_name    = var.cluster_name
   cluster_version = "1.29"
 
   cluster_endpoint_public_access           = true
   enable_cluster_creator_admin_permissions = true
-
-  # cluster_addons = {
-  #   aws-ebs-csi-driver = {
-  #     service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
-  #   }
-  # }
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
   eks_managed_node_group_defaults = {
     ami_type = "AL2_x86_64"
-
   }
 
   eks_managed_node_groups = {
